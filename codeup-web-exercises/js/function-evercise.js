6efa7333a2b80120995392bd77f54a9af2c9a702
cory-tidwell <101146141+cory-tidwell@users.noreply.github.com>
2022-03-14 18:00:53 -0400

    javascript-exercise-update

---
+++ codeup-web-exercises/js/function-evercise.js
@@ -0,0 +1,10 @@
+function printsNameAndAge() {
+
+    var myName = "Cory Tidwell";
+    var myage = 39;
+
+    console.log('My name is: ' + myName + ' and I am ' + myage + ' years old!');
+
+}
+
+printsNameAndAge();
