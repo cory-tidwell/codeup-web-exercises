7133b9b34d85909c926b3fa1c0af19955cfa6990
cory-tidwell <101146141+cory-tidwell@users.noreply.github.com>
2022-03-14 13:12:20 -0400

    javascript-exercises-update

--- codeup-web-exercises/js/external4.js
+++ codeup-web-exercises/js/external4.js
@@ -1,8 +1,10 @@
 var memPremium = prompt('Are you a premium member? (yes/no)');
+var expiredOffer = prompt('Is the offer expired? (yes/no)');
 
-
-if (memPremium == 'yes') {
+if (memPremium == 'yes' && expiredOffer == 'no') {
     alert('There are no item limitations for you!');
-} else {
+} else if (memPremium == 'no' && expiredOffer == 'no') {
     alert('You must purchase 2 or more items!');
+} else {
+    alert('The offer is expired!');
 }
