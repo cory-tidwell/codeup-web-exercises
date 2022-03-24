e23fe95fe058c87999dbedfe851ae7c9b919c5da
cory-tidwell <101146141+cory-tidwell@users.noreply.github.com>
2022-03-24 15:09:06 -0400

    array-exercise

--- codeup-web-exercises/js/planets-array.js
+++ codeup-web-exercises/js/planets-array.js
@@ -56,3 +56,13 @@
     planets.sort();
     console.log(planets);
 })();
+
+
+var array = [1,2,3,4,5];
+console.log(array);
+
+function arrFunc(arr) {
+
+}
+var array2 = array;
+console.log(array2);
