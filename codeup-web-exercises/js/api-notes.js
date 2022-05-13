4e08b5c06477bc7f9e0247fa30835ebd04171cd7
cory-tidwell <101146141+cory-tidwell@users.noreply.github.com>
2022-05-13 11:46:24 -0400

    movie-project-js

--- codeup-web-exercises/js/api-notes.js
+++ codeup-web-exercises/js/api-notes.js
@@ -1,34 +1,107 @@
 "use strict";
 
-    const URL = "https://codeup-json-server.glitch.me/movies";
+    const URL = "https://beryl-chalk-crown.glitch.me/movies";
 
-var movieList = () => {
-   return fetch(URL)
+    // *** RENDER MOVIES ***
+let html = '';
+const displayMovies = () => {
+    return fetch(URL)
         .then(res => res.json())
-        .then(data => {
-            console.log(data);
-            displayMovies(data);
-            // data.forEach(post => {
-                // console.log(post.id);
-                // $('div').append(
-                //     `<p data-id="${post.id}">${post.title} + <img class="poster" src="${post.poster}"> + ${post.director} + ${post.actors}</p>`)
-
-            })
-       .catch(err => console.error(err));
-        // });
+        .then(movies => {
+            console.log(movies);
+            $('#page-loading').replaceWith('');
+            movies.forEach(movie => {
+                html += `<div class="movies-list">
+                        <img class="poster" src="${movie.poster}" alt="a movie poster">
+                        <h5 contenteditable="true" id="edit-title">${movie.title}</h5>
+                        <h6 contenteditable="true" id="edit-rating">Rating: ${movie.rating}</h6>
+                        <button type="button" class="delete" id="${movie.id}">Delete</button>
+                    </div>
+                    <br>`
+            });
+            $('.movie').replaceWith(html)
+        })
+        .catch(error => {
+            console.log(error);
+        });
 }
-fetch('https://codeup-json-server.glitch.me/movies/12',{
-    method: 'DELETE'
-}).then(res=> res.json())
-    .then(data => console.log(data));
-console.log(movieList());
+displayMovies();
 
-const displayMovies = (movies) => {
-    var html = "";
-    movies.forEach(movie => {
-        html += $('div').append(
-            `<p data-id="${movie.id}"> <img class="poster" src="${movie.poster}"> 
-             ${movie.title} + ${movie.director} + ${movie.actors}</p>`)
+// *** POST FUNCTION ***
+$('#add-new-movie').click(function(e) {
+    e.preventDefault();
+    const moviePost = {id: '', title: $('#title').val(), rating: $('#rating').val()};
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(moviePost),
+    };
+         fetch(URL, options)
+        .then(res => res.json())
+        .then(newMovie => {
+            console.log(newMovie);
+               html = `<div class="movie">
+                        <h5>${newMovie.title}</h5>
+                        <h6>Rating: ${newMovie.rating}</h6>
+                    </div>
+                    <br>`
+            $('#movie').append(html);
+        });
+    setTimeout(function (){
+        location.reload();
+    },1000);
 
+});
+
+// *** DELETE FUNCTIONALITY ***
+function deleteMovie(id) {
+     fetch(`${URL}/${id}`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        }
     })
+        .then(response => response.json())
+        .then(() => {
+            console.log(`Deleted: ${id}`)
+        })
+        .catch(console.error)
 }
+// *** DELETE BUTTON FUNCTIONALITY ***
+let timeoutForDelete = setTimeout(function() {
+    $('.delete').click(function(e) {
+        e.preventDefault();
+        var id = $(e.target).attr('id');
+        deleteMovie(id);
+        setTimeout(function (){
+            location.reload();
+        },1000);
+    });
+}, 1000);
+
+//Edit FUNCTION ***
+const editMovie = (movie) => {
+    let options = {
+        method: 'PUT',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(movie)
+    }
+    return fetch(`${URL}`, options).then(resp => resp.json()).then(movie => {
+        $('#edit-title').val(movie.title);
+        $('#edit-rating').val(movie.rating);
+    }).then(displayMovies).catch(err => console.error(err));
+}
+// *** EDIT BUTTON ***f
+$('#update').click(function (e) {
+    e.preventDefault();
+    let title = $('#edit-title').val();
+    let rating = $('#edit-rating').val();
+    let editedMovie = {title, rating};
+    console.log(editedMovie);
+    editMovie(editedMovie);
+    setTimeout(function (){
+        location.reload();
+    },1000);
+});
